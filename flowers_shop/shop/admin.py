5a1e1c23647cd5cca48422ecf32e822091d888eb
Roman Sarnov <rsarnov@mail.ru>
2020-05-22 16:14:05 +0200

    create models

---
+++ flowers_shop/shop/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Product, Category
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'available')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ flowers_shop/shop/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Категории"
+        verbose_name = "Категория"
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    slug = models.SlugField(max_length=100)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def str(self):
+        return self.title
+
