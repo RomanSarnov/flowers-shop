c2da22c3717b4fab273714324fbf655eb6f4aa4f
Oleksandr Kovtunov <kovtunov.prog@gmail.com>
2020-05-28 16:36:24 +0300

    Fix bugs in shop.html and product_detail.html. Refactor code in shop.vies.py

--- flowers_shop/shop/views.py
+++ flowers_shop/shop/views.py
@@ -3,7 +3,6 @@ from django.views import View
 from .models import Product, Category
 
 
-
 # Render shop page
 class ShopRender(View):
     def get(self, request):
@@ -37,11 +36,26 @@ class CategoryRender(View):
 class DiscountProducts(View):
     def get(self, request):
         products = Product.objects.filter(stock__gt=0)
-        return render(request, 'shop/discount.html', context={'products': products})
+
+        context = {
+            'products': products
+        }
+        template = 'shop/discount.html'
+
+        return render(request, template, context)
+
 
 class ProductDetailView(View):
     def get(self, request, slug):
         product = get_object_or_404(Product, slug=slug)
         first_product_image = product.images.first()
         product_images = product.images.all()[1:]
-        return render(request, 'shop/product_detail.html', context={'product': product, 'product_images': product_images, 'first_product_image': first_product_image})
+
+        context = {
+            'product': product,
+            'product_images': product_images,
+            'first_product_image': first_product_image
+        }
+        template = 'shop/product_detail.html'
+
+        return render(request, template, context)
