f2127f40e2a863558218098e55f5291cf372f9a8
Oleksandr Kovtunov <kovtunov.prog@gmail.com>
2020-05-30 20:57:29 +0300

    Merged

--- flowers_shop/shop/urls.py
+++ flowers_shop/shop/urls.py
@@ -4,7 +4,7 @@ from shop.views import *
 
 urlpatterns = [
     path('shop/', ProductListView.as_view(), name='shop'),
-    path('shop/<pk>/', CategoryDetailView.as_view(), name='category'),
+    path('category/<pk>/', CategoryDetailView.as_view(), name='category_detail'),
     path('discount/', DiscountProductsView.as_view(), name='discount'),
     path('product_detail/<slug:slug>/', ProductDetailView.as_view(), name='product_detail')
 ]
