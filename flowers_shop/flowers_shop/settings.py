e8a8a218877a6a116f02a6e50a86bb4da96b17de
Roman Sarnov <rsarnov@mail.ru>
2020-06-19 22:05:07 +0200

    changed

--- flowers_shop/flowers_shop/settings.py
+++ flowers_shop/flowers_shop/settings.py
@@ -147,3 +147,8 @@ try:
     from .local_settings import *
 except ImportError:
     from .prod_settings import *
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
