dcffab2d3c66215e9bef8b49191d7a8fa00b0f56
Roman Sarnov <rsarnov@mail.ru>
2020-06-19 18:24:23 +0200

    deploy

--- flowers_shop/flowers_shop/local_settings.py
+++ flowers_shop/flowers_shop/local_settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = '3xw$wnvhaby0#(olv@o%-78r-w00@p1!k^v3@f23qy()cgenqn'
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
