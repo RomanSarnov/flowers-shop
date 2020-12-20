635c32e79ffeeba874ab26602d3e4fa588e1fa83
Sarnov <roman.sa@itkungfu.ru>
2020-12-20 18:56:32 +0300

    123

--- flowers_shop/flowers_shop/urls.py
+++ flowers_shop/flowers_shop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf .urls.static import static
 
-
+fff = 'saasf'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
